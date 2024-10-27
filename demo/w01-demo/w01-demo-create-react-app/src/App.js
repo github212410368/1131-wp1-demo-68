2f0cca23a19379dca79ab36388fa84d85e6a8005
212410368 <212410368@o365.tku.edu.tw>
2024-10-27 15:24:21 +0800

    W01-p1: node_modules size comparison: create-react-app vs.vite

--- demo/w01-demo/w01-demo-create-react-app/src/App.js
+++ demo/w01-demo/w01-demo-create-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          liu cheng ting<code>,212410368</code>
         </p>
         <a
           className="App-link"
